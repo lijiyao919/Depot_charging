8e590bba06ea29df44a178136806d7e4e4ead6bf
jiyaoli <jiyao.thomas.li@gmail.com>
2023-05-29 02:18:15 -0600

    add info

--- simulator/env.py
+++ simulator/env.py
@@ -6,11 +6,12 @@ class Env:
     def __init__(self):
         self._ev = EV()
         self._reward_model = SimpleReward()
+        self._info = {}
 
     def reset(self):
         Timer.reset()
-        #self._ev.reset()
-        return self._state()
+        self._ev.reset()
+        return self._state(), self._info
 
     def step(self, act):
         self._ev.add_soc(act)
@@ -18,13 +19,13 @@ class Env:
         terminated = Timer.is_end_time()
         if not terminated:
             Timer.tick_time_step()
-        return self._state(), reward, terminated
+        return self._state(), reward, terminated, self._info
 
     def show_performace_metrics(self):
         print("the cost (ec+dc): ", self._ev.total_ec+self._ev.total_dc)
         print("the SoC (kWh): ", self._ev.soc)
     def _state(self):
-        return {"Time": Timer.get_time_step(), "soc":self._ev.soc}
+        return {"Time": Timer.get_time_step(), "soc":self._ev.soc, "max_soc":self._ev.max_soc}
 
 #For test
 if __name__=='__main__':
